a163dd2aef44867829f9f58fc5631bd5fd44c2d1
microcosmos0906 <bhnsk25@gmail.com>
2022-11-25 02:43:51 +0900

    1124_hn_h
    customizing_skintest

--- hwabun/src/main/java/kr/gjai/hwabun/controller/CustomizingController.java
+++ hwabun/src/main/java/kr/gjai/hwabun/controller/CustomizingController.java
@@ -3,12 +3,22 @@ package kr.gjai.hwabun.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import lombok.extern.log4j.Log4j2;
+
 @Controller
+@Log4j2
 public class CustomizingController {
 
 	@RequestMapping("/customizing")
 	public String customizingMain() {
-		
+		log.info("customizing main 확인!!");
 		return "/customizing/customizing";
 	}
+	
+	@RequestMapping("/skintest")
+	public String aiSkinTest() {
+		log.info("skin MBTI 확인!!");
+		return "/customizing/skintest";
+	}
+	
 }
