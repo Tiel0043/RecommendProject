1bf3b96748e3432e1134e78c8eb1c1170e5756b9
rlamw2000 <rlamw2000@naver.com>
2022-11-25 10:16:56 +0900

    commit

--- hwabun/src/main/java/kr/gjai/hwabun/controller/CateController.java
+++ hwabun/src/main/java/kr/gjai/hwabun/controller/CateController.java
@@ -10,8 +10,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import kr.gjai.hwabun.entity.CosmeticsDTO;
 import kr.gjai.hwabun.service.CateService;
+import lombok.extern.log4j.Log4j2;
 
 @Controller
+@Log4j2
 public class CateController { // 대분류 상품 조회 컨트롤러
 	
 	@Autowired
@@ -21,7 +23,6 @@ public class CateController { // 대분류 상품 조회 컨트롤러
 	public String getProductList(@RequestParam(value="cate") String cate,Model model) { // 대분류 입력받는다
 		List<CosmeticsDTO> list = cateService.getProductList(cate);
 		model.addAttribute("cos_list", list);
-		System.out.println(list);
 		return "product/product-list";
 	}
 	
